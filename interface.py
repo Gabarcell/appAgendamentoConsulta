0d354b5218d0828f0f26bda56f49b6237d89ce99
MDRTeles <54379560+MDRTeles@users.noreply.github.com>
2019-12-11 18:54:54 -0300

    Interface final dos sistema

--- interface.py
+++ interface.py
@@ -6,22 +6,27 @@ import sqlite3 as db
 con = db.connect('usuario.db')
 cur = con.cursor()
 
-NOME_APP = "Menu Cadastro"
+NOME_APP = "Controle de Agendamentos"
 root = Tk()
 root.configure(bg='#2F4F4F')
 
 
-
+#tabela cliente
 con = db.connect("usuario.db")
 cur.execute("CREATE TABLE IF NOT EXISTS cliente (nome TEXT, email TEXT)")
+cur.execute("CREATE TABLE IF NOT EXISTS agenda (nome TEXT,datai TEXT, dataf TEXT)")
 cur.close()
 con.commit()
 con.close()
 
-#nam1 = StringVAR()
-#maam2 = StringVar()
+'''
+#tabela de agenda
+cur.execute("CREATE TABLE IF NOT EXISTS agenda (nome TEXT,datai TEXT, dataf TEXT)")
+con.close()
+con.commit()
+con.close()
 
-    
+'''
 
 
 def insert():
@@ -35,8 +40,15 @@ def insert():
     cur.execute(insert, [(nome), (email)])
     con.commit()
    
-    
-
+def put():
+    nome = nam
+    datai = dati
+    dataf = datf
+    con = db.connect("usuario.db")
+    cur = con.cursor()
+    insert = 'INSERT INTO agenda(nome, datai, dataf) VALUES(?,?,?)'
+    cur.execute(insert, [(nome), (datai), (dataf)])
+    con.commit()
 def sair(event=NONE):
     if tkinter.messagebox.askokcancel('Sair', "Deseja realmente sair?"):
         root.destroy()
@@ -55,12 +67,12 @@ def montarMenu():
     cad_menu = Menu(menu_bar, tearoff=0)
     menu_bar.add_cascade(label="Arquivo", underline=0, menu=arq_menu)
     menu_bar.add_cascade(label="Cliente", underline=0, menu=aux_menu)
-    menu_bar.add_cascade(label="Cidade", underline=0, menu=cad_menu)
+    menu_bar.add_cascade(label="Consulta", underline=0, menu=cad_menu)
     root.config(menu=menu_bar)
     arq_menu.add_command(label='Sobre', compound='left', command=showSobre)
     arq_menu.add_separator()
     arq_menu.add_command(label="Sair", accelerator='Alt+F4', compound='left', command=sair)
-    aux_menu.add_command(label='Cliente', compound='left', command=fram_cadCli)
+    aux_menu.add_command(label='Cadastrar', compound='left', command=fram_cadCli)
     aux_menu.add_command(label='Exibir Clientes', compound='left', command=fram_listCli)
     cad_menu.add_command(label='Agenda', compound='left', command=fram_cadCid)
     cad_menu.add_command(label='Exibir Agenda', compound='left', command=fram_listCid)
@@ -124,6 +136,7 @@ def fram_listCli():
     Button(master, text='Submit').grid(row=3,columnspan=2)
 
 def fram_cadCid():
+    global nom, dati, datf
     master = Tk()
 
     master.title("Marcar Consulta")
@@ -132,10 +145,10 @@ def fram_cadCid():
     Label(master, text="Data Consulta:  ").grid(row=2, column=0)
     Label(master, text="Data hoje:  ").grid(row=4, column=0)
     
-    Entry(master).grid(row=0,column=1)
-    Entry(master).grid(row=2, column=1)
-    Entry(master).grid(row=4, column=1)
-    Button(master, text='Submit', command=insert).grid(row=5,columnspan=2)
+    nom =  Entry(master).grid(row=0,column=1)
+    dati = Entry(master).grid(row=2, column=1)
+    datf = Entry(master).grid(row=4, column=1)
+    Button(master, text='Submit', command=put).grid(row=5,columnspan=2)
     Button(master, text='Submit', command=alert).grid(row=5,columnspan=2)
 
 
