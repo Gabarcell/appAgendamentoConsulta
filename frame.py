8216cc340772368b88849871731e4310a1040b9c
Gabarcell <54379815+Gabarcell@users.noreply.github.com>
2019-12-18 00:22:52 -0300

    Interface principal Frame
    A interface principal tem uma barra de menu juntamente das funçoes que direcionam para os cadastros

--- frame.py
+++ frame.py
@@ -1,10 +1,9 @@
 from tkinter import *
 import tkinter.filedialog
-import tkinter.messagebox
+import cli
 
+class Inter:
 
-
-class Inter():
     def __init__(self, master):
 
         self.frame = Frame(master)
@@ -14,7 +13,8 @@ class Inter():
         self.frame.master.protocol('WM_DELETE_WINDOW', self.sair)
         self.frame.master.protocol('WM_DELETE_WINDOW', self.sair)
         self.btn_logout()
-        self.frame.pack()
+
+
         menu_bar = Menu(master)
         arq_menu = Menu(menu_bar, tearoff=0)
         aux_menu = Menu(menu_bar, tearoff=0)
@@ -27,11 +27,14 @@ class Inter():
         arq_menu.add_command(label='Sobre', compound='left', command=self.info)
         arq_menu.add_separator()
         arq_menu.add_command(label="Sair", accelerator='Alt+F4', compound='left', command=self.sair)
-        aux_menu.add_command(label='Cadastrar', compound='left')
-        aux_menu.add_command(label='Exibir Clientes', compound='left')
+        aux_menu.add_command(label='Cadastrar', compound='left',command=self.return_cad)
         cad_menu.add_command(label='Agenda', compound='left')
         cad_menu.add_command(label='Exibir Agenda', compound='left')
 
+    #para entrar no arquivo
+    def return_cad(self):
+        c = cli
+        return c.Cliente(root)
     def btn_logout(self):
         bt = Button(self.frame.master, width=20, text="LOGOUT", bg="yellow",command= self.sair)
         bt.place(x=170, y=340)
@@ -54,24 +57,10 @@ class Inter():
             self.frame.master.destroy()
 
 
-'''
-
-    def printMessage(self):
-        print("Wow, worked!")
-
-        self.form = Entry(instancia)
-        self.form.pack()
-
-        self.calc = Button(self.frame3, text= "Calcule")
-        self.calc.pack()
-'''
+root = Tk()
+Inter(root)
+root.mainloop()
 
 
 
 
-
-
-
-instancia = Tk()
-b = Inter(instancia)
-instancia.mainloop()
